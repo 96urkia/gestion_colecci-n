029b08766646296cf83a4042929a79a826015f25
96urkia <132079059+96urkia@users.noreply.github.com>
2026-06-22 13:55:04 +0200

    Update app.py

--- app.py
+++ app.py
@@ -106,7 +106,20 @@ BIBLIOTECAS = {
     "Aoiz, Agoitz", 2970,
     "Aranguren", 12517,
     "Arbizu", 1126,
-    "Monteagudo", 1200,
+    "Arguedas", 2313,
+    "Arroniz", 1035,
+    "Artajona", 1772,
+    "Artica / Artika", 4848,
+    "Aurizberri / Espinal", 2627,
+    "Ayegui, Aiegi", 2531,
+    "Azagra", 3749,
+    "Barañain", 19575,
+    "Baztan", 7831,
+    "Bera", 3792,
+    "Beriáin", 4129,
+    "Berriozar", 10919,
+    
+    "Monteagudo", 1200
     
     
 }
