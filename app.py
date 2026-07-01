c239d040ddf5f9dd01ff867e4ad7ef10e1075fe4
96urkia <132079059+96urkia@users.noreply.github.com>
2026-07-01 09:05:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -780,7 +780,7 @@ if st.session_state['analizado'] and st.session_state['resultado'] is not None:
     # BLOQUE 2: RECOMENDACIONES DE COMPRA
     # ==========================================
     with pestana_compras:
-        subtab_rec_gen, subtab_rec_cdu, subtab_rec_ml = st.tabs([
+        subtab_rec_gen, subtab_rec_cdu = st.tabs([
             "🌐 A) Recomendaciones Generales", 
             "📚 B) Recomendaciones por CDU"
         ])
